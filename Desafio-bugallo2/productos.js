6e81b3c69bc9bd1195aaf69b925cf89a88b36171
eze_bugi <ezequielbugallo@gmail.com>
2023-05-24 14:29:59 -0300

    entrega desafio 2

---
+++ Desafio-bugallo2/productos.js
@@ -0,0 +1,26 @@
+const product1 = {
+  title: "Producto 1",
+  description: "Descripción del producto 1",
+  price: 10.99,
+  thumbnail: "ruta/imagen1.jpg",
+  code: "ABC123",
+  stock: 5,
+};
+
+const product2 = {
+  title: "Producto 2",
+  description: "Descripción del producto 2",
+  price: 19.99,
+  thumbnail: "ruta/imagen2.jpg",
+  code: "DEF456",
+  stock: 10,
+};
+
+const product3 = {
+  title: "Producto 3",
+  description: "Descripción del producto 3",
+  price: 5.99,
+  thumbnail: "ruta/imagen3.jpg",
+  code: "GHI789",
+  stock: 3,
+};
