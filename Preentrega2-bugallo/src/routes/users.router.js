8058035746dfd5fab64c774581b78b4c515dd8c0
eze_bugi <ezequielbugallo@gmail.com>
2023-07-25 19:42:26 -0300

    reentrega

--- Preentrega2-bugallo/src/routes/users.router.js
+++ Preentrega2-bugallo/src/routes/users.router.js
@@ -1,47 +1,48 @@
-import {Router} from 'express';
-import { userModel } from '../models/user.model.js';
+import { Router } from "express";
+import userModel from "../models/user.model.js";
 
-const router=Router();
+const router = Router();
 
-router.get ('/', async (req,res)=>{
-    try{
-        let users = await userModel.find()
-        res.send({ result : "sucess" ,payload:users})
-    }catch(error){
-        console.log ("No pude traer usuarios " + error)
-    }
+router.get("/", async (req, res) => {
+  try {
+    let users = await userModel.find();
+    res.send({ result: "sucess", payload: users });
+  } catch (error) {
+    console.log("No pude traer usuarios " + error);
+  }
+});
 
-} )
+router.post("/", async (req, res) => {
+  let { first_name, last_name, email } = req.body;
+  if (!first_name || !last_name || !email)
+    res.send({ status: "error", error: " Info Incompleta" });
 
-router.post ('/',async (req,res)=>{
+  let result = await userModel.create({
+    first_name,
+    last_name,
+    email,
+  });
+  res.send({ status: "success", payload: result });
+});
 
-    let{first_name, last_name,email} =req.body;
-    if (!first_name || ! last_name || !email)
-     res.send ({status :"error" , error:" Info Incompleta"})
+router.delete("/:uid", async (req, res) => {
+  let { uid } = req.params;
+  let result = await userModel.deleteOne({ _id: uid });
+  res.send({ status: "success", payload: result });
+});
 
-    let result =await userModel.create({
-        first_name,
-        last_name,
-        email
-    })
-    res.send({status:"success",payload:result})
-})
+router.put("/:uid", async (req, res) => {
+  let { uid } = req.params;
+  let userToReplace = req.body;
+  if (
+    !userToReplace.first_name ||
+    !userToReplace.last_name ||
+    !userToReplace.email
+  )
+    res.send({ status: "error", error: " Info Incompleta" });
 
-router.delete('/:uid',async (req,res)=>{
-    let {uid}=req.params;
-    let result =await userModel.deleteOne({_id:uid})
-    res.send({status:"success",payload:result})
-})
-
-router.put('/:uid',async (req,res)=>{
-    let {uid}=req.params;
-    let userToReplace=req.body; 
-    if(!userToReplace.first_name ||! userToReplace.last_name || !userToReplace.email)
-    res.send ({status :"error" , error:" Info Incompleta"})
-
-    let result= await userModel.updateOne ({_id:uid},userToReplace);
-    res.send({status:"success",payload:result})
-})
-
+  let result = await userModel.updateOne({ _id: uid }, userToReplace);
+  res.send({ status: "success", payload: result });
+});
 
 export default router;
